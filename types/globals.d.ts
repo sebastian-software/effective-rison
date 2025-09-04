9e2213d67246a9ef64c985972cf274aaa69d9d73
Sebastian Werner <s.werner@sebastian-software.de>
2025-09-04 21:52:59 +0200

    refactor: remove UMD/global/CommonJS patterns; convert to pure ES module and drop ambient globals

--- types/globals.d.ts
+++
@@ -1,8 +0,0 @@
-// Ambient declarations for UMD globals used in js/rison.js
-declare var define: any;
-declare var exports: any;
-
-// Augment DOM Window with rison global used by UMD build
-interface Window {
-  rison: any;
-}
